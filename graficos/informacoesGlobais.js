7f40da1c60aeac0c9db13549905d5155a7cf153a
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-10-24 10:15:05 -0300

    d

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,7 +8,7 @@ async function visualizarInformacoesGlobais() {
   paragrafo.classList.add('graficos-container__texto')
   paragrafo.innerHTML = `Você sabia que o mundo tem 
   ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
-  ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média 
+  ${pessoasConectadas} estão conectadas em alguma rede social e passam em média 
   ${dados.tempo_medio} horas conectadas.`
   
   const containder = document.getElementById('graficos-container')
