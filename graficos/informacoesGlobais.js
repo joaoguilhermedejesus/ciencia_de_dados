acced5b550bc289f530aa0af4ba576ae0c56bdd8
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-10-24 10:25:46 -0300

    d

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,6 +6,8 @@ async function visualizarInformacoesGlobais() {
   const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
   const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
   const horas = parseInt (dados.tempo_medio)
+  const minutos = Math.round((dados.tempo_medio - horas) *100)
+  
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
   paragrafo.innerHTML = `Você sabia que o mundo tem 
